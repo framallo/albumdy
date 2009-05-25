8a6d5a15b0faac03b06c82e1417f303446856304
Federico Ramallo <framallo@gmail.com>
2009-05-25 15:26:58 -0500

    ups

--- config/environment.rb
+++ config/environment.rb
@@ -51,7 +51,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-x  # Force all environments to use the same logger level
+  # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
 
